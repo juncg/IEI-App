bab080fb752bdd24b97c526aac5087844c1cca46
juncg <juancarlos.gonzalez.jcgon@gmail.com>
2025-10-02 14:50:38 +0200

    database from files populated and setup code to do so

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+Setup.Main(args);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4515b91913841dc1fa6121607b020cb480ed7be4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7cbb4f8f6b7c2ee7a6ff5b347b271b94df48d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
