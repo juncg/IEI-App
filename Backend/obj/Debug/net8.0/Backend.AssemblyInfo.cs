1177153653580e4ecc0b6b04412c996fb9356644
juncg <juancarlos.gonzalez.jcgon@gmail.com>
2025-10-16 12:49:42 +0200

    ggogle maps

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab080fb752bdd24b97c526aac5087844c1cca46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cfd3a626b297c738e222d58673d2d141dbccf27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
